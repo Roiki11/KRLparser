8948a317bba4176c50723ec7f3eebf15356a4def
Roiki11 <51856064+Roiki11@users.noreply.github.com>
2021-05-28 23:35:09 +0300

    Initial commit

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ from krlLexer import krlLexer
 from krlParser import krlParser
 from krlListener import krlListener
 from krlVisitor import krlVisitor
+import pyparsing as pp
 
+parseLetters = pp.Keyword("A-Z")
+number  = pp.Combine(pp.Word(pp.nums)+pp.Optional(".")+pp.Optional(pp.Word(pp.nums)))            # simple unsigned number
+variable = pp.Char(pp.alphas)          # single letter variable, such as x, z, m, etc.
+arithOp  = pp.oneOf("+ - * /")      # arithmetic operators
+lbrace, rbrace = pp.Suppress('{'), pp.Suppress('}')
+lbracket, rbracket = pp.Suppress('['), pp.Suppress(']')
+
+varAssignment = variable("variable") + "=" + number("value")
+equation = variable + "=" + number + arithOp + number    # will match "x=2+2", etc.
 
 class myListener(krlListener):
 
@@ -14,6 +24,11 @@ class myListener(krlListener):
         self.mainProgram = {}
         self.subPrograms = {}
         self.vars = {}
+        self.subRoutine = False
+
+
+
+
 
     def makeProgram(self, *programs):
         mainProgram = {}
@@ -30,51 +45,86 @@ class myListener(krlListener):
         pass
 
     def enterMainRoutine(self, ctx:krlParser.MainRoutineContext):
-        #print(ctx.getText())
-        print(ctx.getChildCount())
+        pass
 
 
 
     def enterSubRoutine(self, ctx:krlParser.SubRoutineContext):
-        print(ctx.getText())
+        self.subRoutine = True
+
+    def exitSubRoutine(self, ctx:krlParser.SubRoutineContext):
+        self.subRoutine = False
 
     def enterProcedureDefinition(self, ctx:krlParser.ProcedureDefinitionContext):
         n = ctx.getChildCount()
         y = ctx.getChild(1).getText() #procedure name
-        self.mainProgram["name"] = y
-        #for x in range(0, n):
-         #   y=ctx.getChild(x).getText()
-          #  print(y)
-        print(self.mainProgram)
+        if self.subRoutine is False:
+            self.mainProgram["mainName"] = y
+        else:
+            subprog = {"subName": y}
+            self.subPrograms.update(subprog)
+
 
     def enterVariableDeclaration(self, ctx:krlParser.VariableDeclarationContext):
         n=ctx.getChildCount()
-        print(ctx.DECL())
-        print(ctx.type_().getText())
+        #print(ctx.DECL())
+        #print(ctx.type_().getText())
         name = str(ctx.variableName().getText())
-        if name is not 0:
+        if name != 0:
             self.vars[name] = 0
-            print(self.vars)
+            #print(self.vars)
 
     def enterVariableListRest(self, ctx:krlParser.VariableListRestContext):
-        print(ctx.getChildCount())
         for x in range(0, ctx.getChildCount()):
-            print(ctx.getChild(x))
+            #print(ctx.getChild(x))
             y= str(ctx.getText()).strip(",")
             ysp= y.split(",")
             for x in ysp:
-                if x is ",":
+                if x == ",":
                     break
                 else:
                     self.vars[x] = 0
-                    print(self.vars)
 
+                    
+    def enterFormalParameters(self, ctx:krlParser.FormalParametersContext):
 
-class myVisitor(krlVisitor):
-    def visitMainRoutine(self, ctx:krlParser.MainRoutineContext):
         pass
 
+    def enterRoutineDataSection(self, ctx:krlParser.RoutineDataSectionContext):
+        #print(ctx.getText())
 
+        pass
+
+    def enterRoutineImplementationSection(self, ctx:krlParser.RoutineImplementationSectionContext):
+        print(ctx.getChildCount())
+        y = ctx.statementList().getChildCount()
+        lines = []
+        forLoop = []
+        ifLoop = []
+
+        for x in range(0, y):
+            z = str(ctx.statementList().getChild(x).getText())
+            a = z.split()
+            lines.append(a)
+            #print(lines)
+
+        for items in lines:
+            if len(items) == 1:
+                try:
+                    a = items[0]
+                    g = varAssignment.parseString(a)
+                    if g.variable in self.vars:
+                        self.vars[g.variable] = g.value
+
+                except (pp.ParseException) as error:
+                    pass
+            else:
+                if str(items).find("FOR") != -1:
+                    print(items)
+
+
+    def enterStatement(self, ctx:krlParser.StatementContext):
+        pass
 
 
 
@@ -88,8 +138,7 @@ def main():
     printer = myListener()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
-    visitor = myVisitor()
-    visitor.visit(tree)
+    
 
 
 
